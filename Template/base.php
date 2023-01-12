08c514e0c585ad2d3462c542d08b17ec085fd8a8
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-12 19:10:25 +0000

    Created very basic and minimal index and template

---
+++ Template/base.php
@@ -0,0 +1,16 @@
+<!-- SNAPSHOT -->
+<!-- Jan 2023 -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="theme-color" content="#FFF" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <title>SNAPSHOT</title>
+</head>
+<body>
+    <h1>Snapshot</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+//Database Conection
+require_once './database/bootstrap.php';
+require './Template/base.php';
+?>
