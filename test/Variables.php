e6925552ef42c1a949dfb620605242423cd35a80
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-14 18:51:37 +0000

    Created a file that simulate Database entries

---
+++ test/Variables.php
@@ -0,0 +1,52 @@
+<?php
+
+$Posts[0] = array(
+    'ID' => 0,
+    'date' => '10-01-2023',
+    'Username' => 'MarioRossi01',
+    'profilepic' => 'https://thispersondoesnotexist.com/image',
+    'image' => 'https://res.klook.com/image/upload/Mobile/City/afmqgg5h0jl9wnr1dfmf.jpg',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet.',
+    'n_loves' => 140,
+    'n_comments' => 20,
+    'device' => 'iPhone 12 Pro Max',
+    'location' => 'Rome, Italy',
+);
+
+$Posts[1] = array(
+    'ID' => 1,
+    'date' => '17-10-2022',
+    'Username' => 'SofiaBianchiofficial',
+    'profilepic' => 'https://thispersondoesnotexist.com/image',
+    'image' => 'https://images.unsplash.com/photo-1549558549-415fe4c37b60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1719&q=80',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet.',
+    'n_loves' => 41,
+    'n_comments' => 10,
+    'device' => 'iPhone 14 Pro Max',
+);
+
+$Posts[2] = array(
+    'ID' => 2,
+    'date' => '14-10-2022',
+    'Username' => 'CiccioPasticcio',
+    'profilepic' => 'https://thispersondoesnotexist.com/image',
+    'image' => 'https://images.unsplash.com/photo-1586521995659-4204fe01d104?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mnw3MTA5MTU0OHx8ZW58MHx8fHw%3D&w=1000&q=80',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet.',
+    'n_loves' => 85,
+    'n_comments' => 7,
+    'device' => 'iPhone 14 Pro Max',
+);
+
+$Posts[3] = array(
+    'ID' => 3,
+    'date' => '01-10-2022',
+    'Username' => 'MagicAdventurer',
+    'profilepic' => 'https://thispersondoesnotexist.com/image',
+    'image' => 'https://images7.alphacoders.com/411/411820.jpg',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl sit amet.',
+    'n_loves' => 48610,
+    'n_comments' => 1041,
+    'device' => 'iPhone 14 Pro Max',
+);
+
+?>
