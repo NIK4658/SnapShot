5e18327a2a8d9d75f3a45416b21bec5b71e3cb4a
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-14 18:51:51 +0000

    Created "Home" page

---
+++ Home/index.php
@@ -0,0 +1,7 @@
+<?php
+//Database Conection
+require_once("../database/bootstrap.php");
+require_once("../test/Variables.php");
+$templateParams["Type"] = "Home";
+require ("../Template/base.php");
+?>
