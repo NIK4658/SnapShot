611b19cd9eaea9990f0ef4c336d0c028e050873a
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-18 18:52:34 +0000

    Created Profile Page

---
+++ Profile/index.php
@@ -0,0 +1,7 @@
+<?php
+//Database Conection
+require_once("../database/bootstrap.php");
+require_once("../test/Variables.php");
+$templateParams["Type"] = "profile.php";
+require ("../Template/base.php");
+?>
