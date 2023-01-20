f445c86c5cb8e2cf366660eb0e70656b9b382cc7
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-01-20 16:42:37 +0100

    Add function get_password and register account

--- database/database.php
+++ database/database.php
@@ -1,9 +1,8 @@
 <?php
-class DatabaseHelper
-{
+class DatabaseHelper{
     private $db;
-    public function __construct($servername, $username, $password, $dbname)
-    {
+
+    public function __construct($servername, $username, $password, $dbname){
         $this->db = new mysqli($servername, $username, $password, $dbname);
         if ($this->db->connect_error) {
             die("Connection failed: " .$this->db->connect_error);
@@ -12,17 +11,27 @@ class DatabaseHelper
         }
     }
 
-    public function executeQuery($query)
-    {
-        $stmt = $this->db->prepare($query);
+    public function get_password($username){
+        $stmt = $this->db->prepare("SELECT password FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getAllEntries()
-    {
-        return $this->executeQuery("SELECT * FROM TABLENAME");
+    //valutare di eliminare icon, bio, ecc.
+    public function createAccount($username, $password){
+        $query = "INSERT INTO ACCOUNT (username, password, icon, bio, n_posts, n_followers, n_following) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ssssiii',$username, $password, "", "", 0, 0, 0);
+        $stmt->execute();
+        return $stmt->insert_id;
     }
+
+
+
+
+
+
 }
 ?>
