82aacbff2e745af990681e4043e3e737b296c4f0
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-01-21 00:58:37 +0100

    Add create_post and rename different functions with praxis in database.php

--- database/database.php
+++ database/database.php
@@ -11,7 +11,7 @@ class DatabaseHelper{
         }
     }
 
-    public function createAccount($username, $password){
+    public function create_account($username, $password){
         $query = "INSERT INTO ACCOUNT (username, password) VALUES (?, ?)";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('ss',$username, $password);
@@ -76,9 +76,29 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    //NON TESTATA, PROBABILMENTE ERRORI NEL CODICE PHP
+    //aggiungere description, device, location
+    public function create_post($username){
+        $query = "SELECT id FROM POST WHERE username = ? ";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows() == 0){
+            $query = "INSERT INTO POST (username, id) VALUES (?,?)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('si',$username, 1);
+        } else {
+            $query = "INSERT INTO POST (id, username) SELECT MAX(id)+1, ? FROM POST WHERE username = ?";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param('ss',$username, $username);
+        }
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
 
-    //non testata ma dovrebbe funzionare
-    public function get_last_n_images_posts($username, $n){
+     //NON TESTATA MA DOVREBBE FUNZIONARE
+     public function get_last_n_images_posts($username, $n){
         $query = "SELECT id FROM POST WHERE username = ? ORDER BY id DESC LIMIT $n";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param('s',$username);
@@ -86,16 +106,5 @@ class DatabaseHelper{
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
     }
-
-    
-
-
-
-
-
-
-
-
-
 }
 ?>
