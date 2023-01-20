dcb1d08646434ed0af7527879286d23dc52e8251
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-01-20 17:51:44 +0100

    Add functions in database.php

--- database/database.php
+++ database/database.php
@@ -11,7 +11,15 @@ class DatabaseHelper{
         }
     }
 
+    public function createAccount($username, $password){
+        $query = "INSERT INTO ACCOUNT (username, password) VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('ss',$username, $password);
+        $stmt->execute();
+        return $stmt->insert_id;
+    }
 
+//PROFILO
     public function get_account($username){
         $stmt = $this->db->prepare("SELECT * FROM ACCOUNT WHERE username = ?");
         $stmt->bind_param('s',$username);
@@ -28,13 +36,51 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function createAccount($username, $password){
-        $query = "INSERT INTO ACCOUNT (username, password) VALUES (?, ?)";
-        $stmt = $this->db->prepare($query);
-        $stmt->bind_param('ss',$username, $password);
+    public function get_icon($username){
+        $stmt = $this->db->prepare("SELECT icon FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
         $stmt->execute();
-        return $stmt->insert_id;
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    public function get_bio($username){
+        $stmt = $this->db->prepare("SELECT bio FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function get_n_posts($username){
+        $stmt = $this->db->prepare("SELECT n_posts FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function get_n_followers($username){
+        $stmt = $this->db->prepare("SELECT n_followers FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function get_n_following($username){
+        $stmt = $this->db->prepare("SELECT n_following FROM ACCOUNT WHERE username = ?");
+        $stmt->bind_param('s',$username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function get_
+    
+
+
+
 
 
 
