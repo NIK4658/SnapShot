85367e44e88d9a45c494d6cb4bc63e1f120f6aca
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-20 17:02:30 +0000

    SignUp functionality

---
+++ SignUp/index.php
@@ -0,0 +1,6 @@
+<?php
+//Database Conection
+require_once("../database/bootstrap.php");
+$templateParams["Type"] = "SignUp.php";
+require ("../Template/base.php");
+?>
---
+++ Template/SignUp.php
@@ -0,0 +1,13 @@
+<h1>Sign Up</h1>
+<form action="../Operations/SignUp.php" method="post">
+  <label for="username">Username:</label>
+  <input type="text" id="username" name="username"><br><br>
+  <label for="password">Password:</label>
+  <input type="password" id="password" name="password"><br><br>
+  <input type="submit" value="Submit">
+</form>
+<?php
+if(isset($_GET["error"])){
+  echo "Username already taken!";
+}
+?>
