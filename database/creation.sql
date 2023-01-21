490a184f3aec545a7f13083c040884dde277967e
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-01-21 01:02:33 +0100

    Update creation.sql

--- database/creation.sql
+++ database/creation.sql
@@ -3,197 +3,172 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.2              
 -- * Generator date: Sep 14 2021              
--- * Generation date: Fri Jan 13 03:14:39 2023 
+-- * Generation date: Fri Jan 20 19:44:47 2023 
 -- * LUN file: \\Mac\Home\Desktop\Database Snapshot.lun 
--- * Schema: Snapshot/1-1-1 
+-- * Schema: daImplementareNotifiche/1-1-1-1-1 
 -- ********************************************* 
 
 
 -- Database Section
 -- ________________ 
 
-create database Snapshot;
-use Snapshot;
+create database daImplementareNotifiche;
+use daImplementareNotifiche;
 
 
 -- Tables Section
 -- _____________ 
 
-create table POST (
-     account int not null,
-     id int not null,
-     date date not null,
-     image varchar(20) not null,
-     description varchar(100) not null,
-     n_loves int not null,
-     n_comments int not null,
-     device int,
-     location int,
-     constraint IDPOST primary key (account, id));
-
-create table NOTIFICATION (
-     receiver int not null,
-     id int not null,
-     account_post_comment int,
-     post_comment int,
-     id_comment int,
-     account_post_comment2 int,
-     post_comment2 int,
-     comment_comment2 int,
-     comment2 int,
-     date date not null,
-     type int not null,
-     sender int not null,
-     account_post int,
-     id_post int,
-     constraint IDNOTIFICATION primary key (receiver, id),
-     constraint FKprim unique (account_post_comment, post_comment, id_comment),
-     constraint FKsecond unique (account_post_comment2, post_comment2, comment_comment2, comment2));
-
 create table DEVICE (
-     id int not null auto_increment,
      name varchar(20) not null,
-     constraint IDDEVICE_ID primary key (id),
-     constraint IDDEVICE_1 unique (name));
+     constraint IDDEVICE_ID primary key (name));
 
 create table LOCATION (
-     id int not null auto_increment,
      name varchar(20) not null,
-     constraint IDLOCATION_ID primary key (id),
-     constraint IDLOCATION_1 unique (name));
+     constraint IDLOCATION_ID primary key (name));
 
-create table PRIMARY_COMMENT (
-     account_post int not null,
-     post int not null,
+create table POST (
+     username varchar(20) not null,
      id int not null,
-     date date not null,
-     text varchar(50) not null,
-     n_loves int not null,
-     n_comments char(1) not null,
-     account int not null,
-     constraint IDPRIMARY primary key (account_post, post, id));
+     date date default 'CURRENT_TIMESTAMP' not null,
+     description varchar(100) default '''' not null,
+     n_loves int default 0 not null,
+     n_comments int default 0 not null,
+     device varchar(20),
+     location varchar(20),
+     constraint IDPOST primary key (username, id));
 
-create table SECONDARY_COMMENT (
-     account_post int not null,
-     post int not null,
-     comment1 int not null,
+create table COMMENT1 (
+     username_post varchar(20) not null,
+     id_post int not null,
      id int not null,
-     date date not null,
-     text char(1) not null,
-     n_loves char(1) not null,
-     account int not null,
-     constraint IDSECONDARY primary key (account_post, post, comment1, id));
+     date date default 'CURRENT_TIMESTAMP' not null,
+     comment varchar(100) not null,
+     n_loves int default 0 not null,
+     n_comments char(1) default '0' not null,
+     username varchar(20) not null,
+     constraint IDPRIMARY COMMENT primary key (username_post, id_post, id));
 
-create table FOLLOWER (
-     following int not null,
-     account int not null,
-     constraint IDfollower primary key (account, following));
-
-create table LOVE (
-     account_post int not null,
-     post int not null,
-     account int not null,
-     constraint IDLIKE primary key (account_post, post, account));
+create table COMMENT2 (
+     username_post varchar(20) not null,
+     id_post int not null,
+     id_comment int not null,
+     id int not null,
+     date date default 'CURRENT_TIMESTAMP' not null,
+     comment char(100) not null,
+     n_loves char(1) default '0' not null,
+     username varchar(20) not null,
+     constraint IDSECONDARY COMMENT primary key (username_post, id_post, id_comment, id));
 
 create table ACCOUNT (
-     id int not null auto_increment,
      username varchar(20) not null,
-     password varchar(20) not null,
-     icon varchar(20) not null,
-     bio varchar(50) not null,
-     n_posts int not null,
-     n_followers int not null,
-     n_following int not null,
-     constraint IDUSER primary key (id),
-     constraint IDUSER_1 unique (username));
+     password char(20) not null,
+     icon varchar(20) default '''' not null,
+     bio varchar(50) default '''' not null,
+     n_posts int default 0 not null,
+     n_followers int default 0 not null,
+     n_following int default 0 not null,
+     constraint IDUSER primary key (username));
+
+create table FOLLOWER (
+     follower varchar(20) not null,
+     username varchar(20) not null,
+     constraint IDFOLLOWER primary key (username, follower));
+
+create table LOVE_COMMENT1 (
+     username_post varchar(20) not null,
+     id_post int not null,
+     id_comment1 int not null,
+     username varchar(20) not null,
+     constraint IDLOVE_COMMENT1 primary key (username_post, id_post, id_comment1, username));
+
+create table LOVE_COMMENT2 (
+     username_post varchar(20) not null,
+     id_post int not null,
+     id_comment1 int not null,
+     id_comment2 int not null,
+     username varchar(20) not null,
+     constraint IDLOVE_COMMENT2 primary key (username_post, id_post, id_comment1, id_comment2, username));
+
+create table LOVE_POST (
+     username_post varchar(20) not null,
+     id_post int not null,
+     username varchar(20) not null,
+     constraint IDLOVE primary key (username_post, id_post, username));
 
 
 -- Constraints Section
 -- ___________________ 
 
-alter table POST add constraint FKwith
-     foreign key (device)
-     references DEVICE (id);
-
-alter table POST add constraint FKfrom
-     foreign key (location)
-     references LOCATION (id);
-
-alter table POST add constraint FKposted
-     foreign key (account)
-     references ACCOUNT (id);
-
-alter table NOTIFICATION add constraint FKdo
-     foreign key (sender)
-     references ACCOUNT (id);
-
-alter table NOTIFICATION add constraint FKhas
-     foreign key (receiver)
-     references ACCOUNT (id);
-
-alter table NOTIFICATION add constraint FKon
-     foreign key (account_post, id_post)
-     references POST (account, id);
-
-alter table NOTIFICATION add constraint FKprim
-     foreign key (account_post_comment, post_comment, id_comment)
-     references PRIMARY_COMMENT (account_post, post, id);
-
-alter table NOTIFICATION add constraint FKsecond
-     foreign key (account_post_comment2, post_comment2, comment_comment2, comment2)
-     references SECONDARY_COMMENT (account_post, post, comment1, id);
-
 -- Not implemented
 -- alter table DEVICE add constraint IDDEVICE_CHK
 --     check(exists(select * from POST
---                  where POST.device = id)); 
+--                  where POST.device = name)); 
 
 -- Not implemented
 -- alter table LOCATION add constraint IDLOCATION_CHK
 --     check(exists(select * from POST
---                  where POST.location = id)); 
+--                  where POST.location = name)); 
 
--- Not implemented
--- alter table PRIMARY_COMMENT add constraint IDPRIMARY COMMENT_CHK
---     check(exists(select * from NOTIFICATION
---                  where NOTIFICATION.account_post_comment = account_post and NOTIFICATION.post_comment = post and NOTIFICATION.id_comment = id)); 
+alter table POST add constraint FKwith
+     foreign key (device)
+     references DEVICE (name);
 
-alter table PRIMARY_COMMENT add constraint FKrelative
-     foreign key (account_post, post)
-     references POST (account, id);
+alter table POST add constraint FKfrom
+     foreign key (location)
+     references LOCATION (name);
 
-alter table PRIMARY_COMMENT add constraint FKprimm
-     foreign key (account)
-     references ACCOUNT (id);
+alter table POST add constraint FKposted
+     foreign key (username)
+     references ACCOUNT (username);
 
--- Not implemented
--- alter table SECONDARY_COMMENT add constraint IDSECONDARY COMMENT_CHK
---     check(exists(select * from NOTIFICATION
---                  where NOTIFICATION.account_post_comment2 = account_post and NOTIFICATION.post_comment2 = post and NOTIFICATION.comment_comment2 = comment1 and NOTIFICATION.comment2 = id)); 
+alter table COMMENT1 add constraint FKprim com
+     foreign key (username)
+     references ACCOUNT (username);
 
-alter table SECONDARY_COMMENT add constraint FKsecondd
-     foreign key (account)
-     references ACCOUNT (id);
+alter table COMMENT1 add constraint FKrelative post
+     foreign key (username_post, id_post)
+     references POST (username, id);
 
-alter table SECONDARY_COMMENT add constraint FKrelativee
-     foreign key (account_post, post, comment1)
-     references PRIMARY_COMMENT (account_post, post, id);
+alter table COMMENT2 add constraint FKsecond com
+     foreign key (username)
+     references ACCOUNT (username);
+
+alter table COMMENT2 add constraint FKrelative comment
+     foreign key (username_post, id_post, id_comment)
+     references COMMENT1 (username_post, id_post, id);
 
 alter table FOLLOWER add constraint FKfollow
-     foreign key (account)
-     references ACCOUNT (id);
+     foreign key (username)
+     references ACCOUNT (username);
 
 alter table FOLLOWER add constraint FKfollowed
-     foreign key (following)
-     references ACCOUNT (id);
+     foreign key (follower)
+     references ACCOUNT (username);
 
-alter table LOVE add constraint FKlik_USE
-     foreign key (account)
-     references ACCOUNT (id);
+alter table LOVE_COMMENT1 add constraint FKLOV_ACC
+     foreign key (username)
+     references ACCOUNT (username);
 
-alter table LOVE add constraint FKlik_POS
-     foreign key (account_post, post)
-     references POST (account, id);
+alter table LOVE_COMMENT1 add constraint FKLOV_PRI
+     foreign key (username_post, id_post, id_comment1)
+     references COMMENT1 (username_post, id_post, id);
+
+alter table LOVE_COMMENT2 add constraint FKR_1_ACC
+     foreign key (username)
+     references ACCOUNT (username);
+
+alter table LOVE_COMMENT2 add constraint FKR_1_SEC
+     foreign key (username_post, id_post, id_comment1, id_comment2)
+     references COMMENT2 (username_post, id_post, id_comment, id);
+
+alter table LOVE_POST add constraint FKlik_ACC
+     foreign key (username)
+     references ACCOUNT (username);
+
+alter table LOVE_POST add constraint FKlik_POS
+     foreign key (username_post, id_post)
+     references POST (username, id);
 
 
 -- Index Section
