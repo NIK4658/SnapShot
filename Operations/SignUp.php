14d707952d349e8f07e5329249122b561fc5fe09
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-21 18:56:01 +0000

    Fixed query name

--- Operations/SignUp.php
+++ Operations/SignUp.php
@@ -9,7 +9,7 @@ if (isset($_POST["username"]) && isset($_POST["password"]) && $_POST["username"]
     if (count($account) == 0) {
         //Check password length
         if(strlen($_POST["password"])<=16 && strlen($_POST["password"])>=8){
-            $dbh->createAccount($_POST["username"], $_POST["password"]);
+            $dbh->create_account($_POST["username"], $_POST["password"]);
             $_SESSION["username"] = $_POST["username"];
             $error=0;
             if (!file_exists('../uploads/'.$_SESSION["username"].'/')) {
