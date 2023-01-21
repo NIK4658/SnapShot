cf7fc16d6ef5d37738c0ca9720e9d8a29e1ce812
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-21 19:00:09 +0000

    Added js param to home

--- Home/index.php
+++ Home/index.php
@@ -3,5 +3,6 @@
 require_once("../database/bootstrap.php");
 require_once("../test/Variables.php");
 $templateParams["Type"] = "home.php";
+$templateParams["Script"] = "home.js";
 require ("../Template/base.php");
 ?>
