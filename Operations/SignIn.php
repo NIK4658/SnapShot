01d73e3514850dfe52c1212a9c0d9af09c3fd2ca
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-22 17:40:16 +0000

    Little Refactoring

--- Operations/SignIn.php
+++ Operations/SignIn.php
@@ -1,27 +1,25 @@
 <?php
-session_start();
-require_once("../database/bootstrap.php");
-
-if (isset($_POST["username"]) && isset($_POST["password"]) && $_POST["username"] != "" && $_POST["password"] != "") {
-    $account = $dbh->get_account($_POST["username"]);
-    if (count($account) != 0) {
-        $password = $account[0]["password"];
-        if ($password == $_POST["password"]) {
-            $_SESSION["username"] = $account[0]["username"];
-        }else{
-            $error=1;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    session_start();
+    require_once("../database/bootstrap.php");
+    $error = 1;
+    //Check if fields are empty
+    if (isset($_POST["username"]) && isset($_POST["password"]) && $_POST["username"] != "" && $_POST["password"] != "") {
+        $account = $dbh->get_account($_POST["username"]);
+        //Check if the user exists
+        if (count($account) != 0) {
+            $password = $account[0]["password"];
+            //Check if the password is correct
+            if ($password == $_POST["password"]) {
+                $_SESSION["username"] = $account[0]["username"];
+                $error = 0;
+            }
         }
     }
-    else{
-        $error=1;
+    if ($error == 0) {
+        header("Location: /SnapShot/");
+    } else {
+        header("Location: ../SignIn?error=1");
     }
-    $error=0;
-}else{
-    $error=1;
-}
-if($error==0){
-    header("Location: /SnapShot/");
-}else{
-    header("Location: ../SignIn?error=1");
 }
 ?>
