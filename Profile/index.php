91fe211459178bc13f728edfba4af7a8a6c18f02
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-23 17:28:40 +0000

    Profile check the presence of one username

--- Profile/index.php
+++ Profile/index.php
@@ -4,5 +4,10 @@ require_once("../database/bootstrap.php");
 require_once("../test/Variables.php");
 $templateParams["Type"] = "profile.php";
 $templateParams["NeedLogin"] = true;
+if (isset($_GET["username"])) {
+    $templateParams["account"] = $dbh->get_account($_GET["username"]);
+}else{
+    echo "Error";
+}
 require ("../Template/base.php");
 ?>
