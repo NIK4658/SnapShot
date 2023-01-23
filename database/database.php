41a1431081ec9b939165a34c19f3ca167397a71b
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-01-23 17:41:31 +0100

    Fix bugs in get_last_n_post_from in DatabaseHelper

--- database/database.php
+++ database/database.php
@@ -135,19 +135,15 @@ class DatabaseHelper{
     }
 
     //NON TESTATA
+    //Return n last post from i included, n>0 and i>0
     public function get_last_n_posts_from($username, $n, $i){
         $query = "SELECT * FROM POST WHERE username = ? AND id BETWEEN ? AND ? ORDER BY id DESC";//ordinare by data
         $stmt = $this->db->prepare($query);
-        $to = $i + $n;
+        $to = $i + $n - 1;
         $stmt->bind_param('sii', $username, $i, $to);
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC);
-    }
-    
-    //NON TESTATA
-    public function get_last_n_posts($username, $n){
-        $this->get_last_n_posts_from($username, $n, 0);
     }
 
 }
