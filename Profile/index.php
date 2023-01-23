bfe1317e2621819b2fc1e5e9e875f555248ea7af
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-23 18:23:17 +0000

    Profile Page

--- Profile/index.php
+++ Profile/index.php
@@ -1,11 +1,10 @@
 <?php
 //Database Conection
 require_once("../database/bootstrap.php");
-require_once("../test/Variables.php");
 $templateParams["Type"] = "profile.php";
 $templateParams["NeedLogin"] = true;
 if (isset($_GET["username"])) {
-    $templateParams["account"] = $dbh->get_account($_GET["username"]);
+    $PageParams["account"] = $dbh->get_account($_GET["username"]);
 }else{
     echo "Error";
 }
--- Template/profile.php
+++ Template/profile.php
@@ -3,29 +3,27 @@ require_once("../database/bootstrap.php");
 require_once("../Operations/getImage.php");
 
 if(isset($_GET["username"])){
-    $templateParams["account"] = $dbh->get_account($_GET["username"]);
-    $personal = ($templateParams["account"][0]["username"] == $_SESSION["username"]);
+    $PageParams["account"] = $dbh->get_account($_GET["username"]);
+    $personal = ($PageParams["account"][0]["username"] == $_SESSION["username"]);
     if($personal){
         echo "<button class=mainbutton onclick=location.href='/SnapShot/Operations/SignOut.php'>SignOut</button>";
     }
 
-    echo "<p> Username: " . $templateParams["account"][0]["username"]."</p>";
-    echo "<p> Bio: " . $templateParams["account"][0]["bio"]."</p>";
-    echo "<p> Password: " . $templateParams["account"][0]["password"]."</p>";
-    echo "<p> n_post: " . $templateParams["account"][0]["n_posts"]."</p>";
-    echo "<p> n_followers: " . $templateParams["account"][0]["n_followers"]."</p>";
-    echo "<p> n_following: " . $templateParams["account"][0]["n_following"]."</p>";
+    echo "<p> Username: " . $PageParams["account"][0]["username"]."</p>";
+    echo "<p> Bio: " . $PageParams["account"][0]["bio"]."</p>";
+    echo "<p> Password: " . $PageParams["account"][0]["password"]."</p>";
+    echo "<p> n_post: " . $PageParams["account"][0]["n_posts"]."</p>";
+    echo "<p> n_followers: " . $PageParams["account"][0]["n_followers"]."</p>";
+    echo "<p> n_following: " . $PageParams["account"][0]["n_following"]."</p>";
 
 
-    $posts = $dbh->get_last_n_posts_from($templateParams["account"][0]["username"], 10, 0);
-
+    $posts = $dbh->get_last_n_posts_from($PageParams["account"][0]["username"], 10, 0);
     foreach ($posts as $post){
-        $image=getImage($templateParams["account"][0]["username"], $post["id"]);
+        $image=getImage($PageParams["account"][0]["username"], $post["id"]);
         if($image!="Error"){
-            echo "<img src=".$image."></img>";
+            echo "<a href=../Post?username=".$PageParams["account"][0]["username"]."&postID=".$post["id"]."><img src=".$image."></img></a>";
         }
     }
-
 
 }else{
     echo "Error";
