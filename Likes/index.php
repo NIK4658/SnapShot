18ec5134a06956a69eea9d1eac46ec13a89d0e23
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-25 17:36:20 +0000

    Implemented likes page

---
+++ Likes/index.php
@@ -0,0 +1,13 @@
+<?php
+//Database Conection
+require_once("../database/bootstrap.php");
+$templateParams["Type"] = "likes.php";
+$templateParams["NeedLogin"] = true;
+if (isset($_GET["username"]) && isset($_GET["postID"])) {
+    $PageParams["account"] = $dbh->get_account($_GET["username"]);
+    $PageParams["post"]= $dbh->get_post($_GET["username"], $_GET["postID"] );
+}else{
+    echo "Error";
+}
+require ("../Template/base.php");
+?>
