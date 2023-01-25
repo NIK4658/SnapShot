2b4694bdf0b05d3214b083ef56bc2ef3df157b7d
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-01-25 17:42:41 +0000

    Added follow and unfollow buttons to the page

--- Template/follows.php
+++ Template/follows.php
@@ -4,12 +4,17 @@ require_once("../database/bootstrap.php");
 $accounts = $dbh->get_account1();
 
 foreach ($accounts as $account) {
-    // if($_SESSION["username"]===$account["username"]){
-    //     echo "<p>".$account["username"]."</p>";
-    // }else{
-    //     echo "<a href=/SnapShot/Profile?username=".$account["username"].">".$account["username"]."</a><br>";
-    // }
+    echo "<a href=/SnapShot/Profile?username=".$account["username"].">".$account["username"]."</a>";
 
-    echo "<a href=/SnapShot/Profile?username=".$account["username"].">".$account["username"]."</a><br>";
+    if($_SESSION["username"] != $account["username"]){
+        if($dbh->is_following($_SESSION["username"], $account["username"])){
+            $content="UnFollow";
+        } else {
+            $content = "Follow";
+        }
+        echo "<button onclick=location.href='/SnapShot/Operations/Follows.php?user1=".$_SESSION["username"]."&user2=".$account["username"]."'>".$content."</button></br>";
+    }else{
+        echo "</br>";
+    }
 }
 ?>
