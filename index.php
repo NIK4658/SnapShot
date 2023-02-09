3dbde77e6a5f4d4723faf699965cba99b2eff15e
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-02-09 22:05:04 +0000

    Changed js files Home

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ require_once 'app/check_session.php';
 
 $templateParams["title"] = "Home";
 $templateParams["scripts"] = "
+    <script src='js/start_feed.js'  type='module'></script>
     <script src='js/feed.js' type='module'></script>
-    <script src='js/post.js'></script>
+    <script src='js/post.js' ></script>
 ";
 $templateParams["page"] = "home_page.php";
 require_once 'templates/base.php';
