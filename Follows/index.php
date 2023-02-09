c462710e3e57d746634f8c6c8502ca61dda87163
FedericoMuccioli <federicomuccioliabc@gmail.com>
2023-02-09 16:41:20 +0100

    Refactoring

--- Follows/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Database Conection
-require_once("../database/bootstrap.php");
-$templateParams["Type"] = "follows.php";
-$templateParams["NeedLogin"] = true;
-require ("../Template/base.php");
-?>
