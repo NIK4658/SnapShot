d798c69d0e60df6fd45cd96d11938f32c94d7361
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2023-02-13 21:18:06 +0000

    Refactoring

--- js/single_post.js
+++ js/single_post.js
@@ -11,8 +11,6 @@ function getSinglePost(postid) {
         let post = result.post;
         let currentUsername = result.currentUsername;
         const MainPageDiv = document.getElementById("post-page");
-        console.log(post[0].location);
-        console.log(post[0].device);
         if (post != 0) {
             postDiv = getPostContainer(post[0].post_id, post[0].username, post[0].caption, result.isLiked, post[0].rated, post[0].location, post[0].device);
             MainPageDiv.appendChild(postDiv);
